85b340ed2ad0003a628fdf5c07709bf3e0e750a5
Naufal Gita Mahardika <96105558+Nopxz@users.noreply.github.com>
2022-01-04 13:50:20 +0700

    Add files via upload

---
+++ src/main/java/KonversiBeratFinal_20200140124/KonversiBeratFinal/KonversiBeratFinalApplication.java
@@ -0,0 +1,13 @@
+package KonversiBeratFinal_20200140124.KonversiBeratFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonversiBeratFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonversiBeratFinalApplication.class, args);
+	}
+
+}
